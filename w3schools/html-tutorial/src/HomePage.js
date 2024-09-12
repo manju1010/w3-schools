a348dbaec6517699f6ac96b5101ee533c460aea2
manju1010 <manjcs081@rmkcet.ac.in>
2024-09-12 21:30:40 +0530

    commit

--- w3schools/html-tutorial/src/HomePage.js
+++ w3schools/html-tutorial/src/HomePage.js
@@ -1,42 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; 
-import { faCaretDown } from "@fortawesome/free-solid-svg-icons"; 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretDown, faCircleHalfStroke } from "@fortawesome/free-solid-svg-icons";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons/faMagnifyingGlass";
+import { faMagicWandSparkles } from "@fortawesome/free-solid-svg-icons";
+import { faCode, faCartShopping } from "@fortawesome/free-solid-svg-icons"; 
 import "./HomePage.css";
 
 const HomePage = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
   return (
-    <div className="home-page">
+    <div className={`home-page ${isDarkMode ? "dark-mode" : "light-mode"}`}>
       <header className="home-header">
         <div className="header-left">
           <Link to="/">
             <div className="w3-school-logo">
-              <img 
-                src="https://upload.wikimedia.org/wikipedia/commons/a/a0/W3Schools_logo.svg" 
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/a/a0/W3Schools_logo.svg"
                 alt="W3Schools Logo"
               />
             </div>
           </Link>
 
           <Link to="/tutorial" className="tutorial-link">
-  <span>Tutorial</span>
-  <FontAwesomeIcon icon={faCaretDown} className="dropdown-icon" />
-</Link>
-<Link to="/Excercise" className="Excercise-link">
-  <span>Excercise</span>
-  <FontAwesomeIcon icon={faCaretDown} className="dropdown-icon" />
-</Link>
-<Link to="/Certificates" className="Certificates-link">
-  <span>Certificates</span>
-  <FontAwesomeIcon icon={faCaretDown} className="dropdown-icon" />
-</Link>
-<Link to="/services" className="service-link">
-  <span>Services</span>
-  <FontAwesomeIcon icon={faCaretDown} className="dropdown-icon" />
-</Link>
+            <span>Tutorial</span>
+            <FontAwesomeIcon icon={faCaretDown} className="dropdown-icon" />
+          </Link>
+          <Link to="/exercise" className="exercise-link">
+            <span>Exercise</span>
+            <FontAwesomeIcon icon={faCaretDown} className="dropdown-icon" />
+          </Link>
+          <Link to="/certificates" className="certificates-link">
+            <span>Certificates</span>
+            <FontAwesomeIcon icon={faCaretDown} className="dropdown-icon" />
+          </Link>
+          <Link to="/services" className="service-link">
+            <span>Services</span>
+            <FontAwesomeIcon icon={faCaretDown} className="dropdown-icon" />
+          </Link>
 
-<input type="text" placeholder="Search..." className="search-input" /> 
+          <div className="search">
+            <input
+              type="text"
+              placeholder="Search..."
+              value={searchQuery}
+              onChange={handleSearchChange}
+              aria-label="Search"
+            />
+            <FontAwesomeIcon icon={faMagnifyingGlass} className="search-icon" />
+          </div>
 
+          <div className="toggle-dark-light" onClick={toggleDarkMode}>
+            <FontAwesomeIcon icon={faCircleHalfStroke} />
+          </div>
+        </div>
+
+        <div className="header-right">
+          <Link to="/plus" className="plus-link btn"> 
+            <div className="plus">
+              <FontAwesomeIcon icon={faMagicWandSparkles} />
+              Plus
+            </div>
+          </Link>
+
+          <Link to="/code" className="code-link btn"> 
+            <div className="code">
+              <FontAwesomeIcon icon={faCode} />
+              Code
+            </div>
+          </Link>
+
+          <Link to="/cart" className="cart-link btn"> 
+            <div className="cart">
+              <FontAwesomeIcon icon={faCartShopping} />
+              Cart
+            </div>
+          </Link>
         </div>
       </header>
     </div>
