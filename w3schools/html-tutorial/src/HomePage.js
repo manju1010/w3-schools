1c3f2f796224edc78bf26c076a8dea54b56d4de0
manju1010 <manjcs081@rmkcet.ac.in>
2024-09-12 14:51:14 +0530

    commit

--- w3schools/html-tutorial/src/HomePage.js
+++ w3schools/html-tutorial/src/HomePage.js
@@ -1,22 +1,44 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; 
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons"; 
+import "./HomePage.css";
 
 const HomePage = () => {
   return (
-    <div>
-      <header>
-        <h1>Learn to code</h1>
-        <p>with the world's largest web developer site</p>
-        <input type="search" placeholder="Search our tutorials, e.g., HTML" />
+    <div className="home-page">
+      <header className="home-header">
+        <div className="header-left">
+          <Link to="/">
+            <div className="w3-school-logo">
+              <img 
+                src="https://upload.wikimedia.org/wikipedia/commons/a/a0/W3Schools_logo.svg" 
+                alt="W3Schools Logo"
+              />
+            </div>
+          </Link>
+
+          <Link to="/tutorial" className="tutorial-link">
+  <span>Tutorial</span>
+  <FontAwesomeIcon icon={faCaretDown} className="dropdown-icon" />
+</Link>
+<Link to="/Excercise" className="Excercise-link">
+  <span>Excercise</span>
+  <FontAwesomeIcon icon={faCaretDown} className="dropdown-icon" />
+</Link>
+<Link to="/Certificates" className="Certificates-link">
+  <span>Certificates</span>
+  <FontAwesomeIcon icon={faCaretDown} className="dropdown-icon" />
+</Link>
+<Link to="/services" className="service-link">
+  <span>Services</span>
+  <FontAwesomeIcon icon={faCaretDown} className="dropdown-icon" />
+</Link>
+
+<input type="text" placeholder="Search..." className="search-input" /> 
+
+        </div>
       </header>
-      <nav>
-        <ul>
-          <li><Link to="/html">HTML</Link></li>
-          <li><Link to="/css">CSS</Link></li>
-          <li><Link to="/javascript">JavaScript</Link></li>
-          <li><Link to="/python">Python</Link></li>
-        </ul>
-      </nav>
     </div>
   );
 };
